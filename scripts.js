33b785ef333d382f50e6f40b891423b0c33044da
isaacfonseca150@gmail.com <isaacfonseca150@gmail.com>
2024-09-18 15:16:55 -0300

    first commit

--- scripts.js
+++ scripts.js
@@ -1,17 +1,19 @@
-let firstNumber = prompt('Digite o primeiro número')
-let secondNumber = prompt('Digite o segundo número')
+let student = prompt('Qual o nome do(a) aluno(a)?')
+let n1 = prompt('Qual a nota da primeira prova?')
+let n2 = prompt('Qual a nota da segunda prova?')
+let n3 = prompt('Qual a nota da terceira prova?')
 
-firstNumber = Number(firstNumber)
-secondNumber = Number(secondNumber)
 
-const sum = firstNumber + secondNumber
-const sub = firstNumber - secondNumber
-const mult = firstNumber * secondNumber
-const div = firstNumber / secondNumber
-const restdiv = firstNumber % secondNumber
+let average = (Number(n1) + Number(n2) + Number(n3)) / 3
 
-alert('soma: ' + sum)
-alert('subtração: ' + sub)
-alert('multiplicação: ' + mult)
-alert('divisão: ' + div)
-alert('resto da divisão: ' + restdiv)
+let result = average > 6 
+
+average = average.toFixed(2)
+
+if (result) {
+    alert('Parabéns, ' + student + '! Sua média foi de: ' + average)
+}   else if (average < 3) {
+    alert('Reprovado')
+}   else {
+    alert(student + 'Estude para sua prova de recuperação! ' + average)
+}
